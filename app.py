767970689f524b6501e42347f5eeb3a8f748b8e7
Alex <54858955+AlexLiu1975@users.noreply.github.com>
2025-08-03 12:41:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -73,4 +73,5 @@ def webhook():
     return "OK", 200
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render 會提供 PORT 環境變數
+    app.run(host="0.0.0.0", port=port)
